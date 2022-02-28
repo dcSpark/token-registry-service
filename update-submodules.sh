854a3eff4506031b6a6bc5e7be7a6267c0a10bf4
Tomasz Szandala <Tomasz.Szandala@gmail.com>
2022-02-28 16:44:59 +0100

    Added package.json update

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+git pull
+
+readonly LATEST_TAG=$(git tag | sort -V | tail -1)
+readonly PATCH=${LATEST_TAG##*.}
+readonly NEW_PATCH=$(( PATCH + 1 ))
+
 git submodule update --init --recursive --remote
+sed -ri "s/(\"version\": )\"0.0.6\"/\1\"${LATEST_TAG%.*}.${NEW_PATCH}\"/" package.json
 
 git commit -am"Automatic updating registries
 
 Done by: ${USER}
 On host: ${HOSTNAME}"
-
-readonly LATEST_TAG=$(git tag | sort -V | tail -1)
-readonly PATCH=${LATEST_TAG##*.}
-readonly NEW_PATCH=$(( PATCH + 1 ))
 
 echo "$(date) tagging as ${LATEST_TAG%.*}.${NEW_PATCH}"
 
